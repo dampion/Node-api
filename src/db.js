7b29cdcb29293389f47daf57105a9237a6bc5713
dampion <evanchen79@gmail.com>
2021-04-18 01:44:16 +0800

    [Change] prototype api

--- src/db.js
+++ src/db.js
@@ -1,20 +1,29 @@
-// ongoose func lib
+// Require the mongose library
 const mongoose = require('mongoose');
 
 module.exports = {
   connect: DB_HOST => {
+    // Use the Mongo driver's updated URL string parser
     mongoose.set('useNewUrlParser', true);
-    mongoose.set('useFindAndModify', false)
-    mongoose.set('useCreateIndex', true)
-    mongoose.set('useUnifiedToplogy', true)
-    mongoose.connect(DB_HOST)
+    // Use `findOneAndUpdate()` in place of findAndModify()
+    mongoose.set('useFindAndModify', false);
+    // Use `createIndex()` in place of `ensureIndex()`
+    mongoose.set('useCreateIndex', true);
+    // Use the new server discovery & monitoring engine
+    mongoose.set('useUnifiedTopology', true);
+    // Connect to the DB
+    mongoose.connect(DB_HOST);
+    // Log an error if we fail to connect
     mongoose.connection.on('error', err => {
-      console.error(err)
-      console.log('Mongodb connection error. Please make sure mongodb is running.')
+      console.error(err);
+      console.log(
+        'MongoDB connection error. Please make sure MongoDB is running.'
+      );
       process.exit();
     });
   },
+
   close: () => {
-    mongoose.connection.close()
+    mongoose.connection.close();
   }
-}
+};
